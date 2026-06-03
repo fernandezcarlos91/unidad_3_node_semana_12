7b04ce3f37fc15a95d1ef7f8e8a4fbd51b43d781
Carlos <fernandez.noriega.cf@gmail.com>
2026-06-02 21:58:33 -0400

    Tarea corregida

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const logger = require('./middlewares/logger');
-/*const usersRouter = require('./routes/users');*/
+const usersRouter = require('./routes/users');
 const personajes_de_marvel_que_no_salen_en_peliculas_de_marvelRouter = require('./routes/personajes_de_marvel_que_no_salen_en_peliculas_de_marvel');
 const app = express();
 
@@ -9,8 +9,8 @@ app.use(express.json()); // leer JASON del body
 app.use(logger);
 
 // rutas
-/*app.use('/usuario', usersRouter);*/
-app.use('/usuario', personajes_de_marvel_que_no_salen_en_peliculas_de_marvelRouter);
+app.use('/usuario', usersRouter);
+app.use('/personaje', personajes_de_marvel_que_no_salen_en_peliculas_de_marvelRouter);
 
 const PORT = 3002;
 
